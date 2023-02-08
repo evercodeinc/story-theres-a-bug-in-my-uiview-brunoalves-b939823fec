6af1ea8a2a7ec6de58513340ab0ec7da0dda686d
Brun41v35 <alves.bruno945@gmail.com>
2023-02-08 11:38:43 -0300

    Add new property

--- GitHubApp/Screens/Components/LoadingView.swift
+++ GitHubApp/Screens/Components/LoadingView.swift
@@ -2,35 +2,37 @@ import UIKit
 
 final class LoadingView: UIView {
     
+    // MARK - Private Properties
+    
     private lazy var activityIndicatorView: UIActivityIndicatorView = {
-      
         let view = UIActivityIndicatorView(style: .large)
         view.translatesAutoresizingMaskIntoConstraints = false
+        view.isHidden = false
         return view
     }()
     
+    // MARK - Init
+    
     init() {
         super.init(frame: .zero)
-        
         self.backgroundColor = .white
-        
         addSubviews()
         configureConstraints()
     }
     
+    @available(*,unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // MARK - Internal Methods
+    
     func addSubviews() {
-        
         addSubview(activityIndicatorView)
     }
     
     func configureConstraints() {
-        
         NSLayoutConstraint.activate([
-        
             activityIndicatorView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
             activityIndicatorView.centerYAnchor.constraint(equalTo: self.centerYAnchor)
         ])
